1ed6ed6400257ca4f1e776e8f965280b38b81956
Dave Hocker <AtHomeX10@gmail.com>
2019-04-12 14:30:32 -0500

    Build new state machine for managing PIR sensor

--- pir_sensor_thread.py
+++ pir_sensor_thread.py
@@ -38,13 +38,23 @@ class SensorThread(threading.Thread):
 
     The initial state is unknown.
     """
-    def __init__(self, pir_pin=12, name="PIRSensorThread", notify=None):
+
+    # Sensor states
+    _state_init = 0
+    _state_on = 1
+    _state_off = 2
+    _state_count_off = 3
+    _state_count_on = 4
+
+    def __init__(self, pir_pin=12, name="PIRSensorThread", notify=None, time_off=300, time_on=2):
         """
         Class constructor.
         :param pir_pin: board pin number where PIR sensor data line is connected.
         :param name: A human readable name for the thread.
         :param notify: Callback for handling sensor state. This method will
         be called on the sensor thread, NOT the current thread.
+        :param time_off: The count down value for going to the off state
+        :param time_on: The counte down value for going to the on state
         """
         threading.Thread.__init__(self, name=name)
         # Using pin 12 (GPIO 18) for PIR sensor signal
@@ -58,7 +68,15 @@ class SensorThread(threading.Thread):
         self._terminate_thread = False
 
         # Public properties
+        # The debounced sensor value (not necessarily the actual sensor value)
         self.sensor_value = 0
+
+        # Sensor value state machine
+        self._time_off = time_off
+        self._time_on = time_on
+        self._sensor_state = self._state_init
+        self._count_down_off = self._time_off
+        self._count_down_on = self._time_on
 
     def run(self):
         """
@@ -87,10 +105,8 @@ class SensorThread(threading.Thread):
 
         try:
             while not self._terminate_thread:
-                # Read the current state of the PIR sensor.
-                # 0 = no movement detected
-                # 1 = movement detected
-                self.sensor_value = GPIO.input(self.pir_pin)
+                # Update the current state of the PIR sensor.
+                self._update_sensor()
 
                 if self._notify_proc:
                     self._notify_proc(self.sensor_value)
@@ -101,3 +117,74 @@ class SensorThread(threading.Thread):
         except Exception as ex:
             logger.error("PIR Sensor thread terminated by unhandled exception")
             logger.error(ex)
+
+    def _update_sensor(self):
+        """
+        Update the sensor state machine based on the current value of the sensor.
+        The whole purpose of the state machine is to debounce the sensor value,
+        thus avoiding false positives or negatives.
+        :return: The debounced value of the sensor, true or false.
+        """
+        # Read the actual state of the PIR sensor.
+        # 0 = no movement detected
+        # 1 = movement detected
+        actual_sensor_value = GPIO.input(self.pir_pin)
+
+        # This is the state machine
+        if self._sensor_state == self._state_init:
+            # Use the actual sensor value to set the initial state
+            if actual_sensor_value:
+                self._sensor_state = self._state_on
+                self.sensor_value = True
+            else:
+                self._sensor_state = self._state_off
+                self.sensor_value = False
+        elif self._sensor_state == self._state_on:
+            # Current state is on
+            if actual_sensor_value:
+                # No state change
+                pass
+            else:
+                # New state is counting down to off
+                self._sensor_state = self._state_count_off
+                self._count_down_off = self._time_off
+        elif self._sensor_state == self._state_off:
+            # Current state is off
+            if actual_sensor_value:
+                # New state is count down to on
+                self._sensor_state = self._state_count_on
+                self._count_down_on = self._time_on
+            else:
+                # No state change
+                pass
+        elif self._sensor_state == self._state_count_on:
+            # Current state is counting down to on
+            self._count_down_on -= 1
+            if actual_sensor_value == self._state_off:
+                # New state is off
+                self._sensor_state = self._state_off
+            elif self._count_down_on > 0:
+                # State remains count down to on
+                pass
+            else:
+                # New state is on
+                self._sensor_state = self._state_on
+                self.sensor_value = True
+        elif self._sensor_state == self._state_count_off:
+            # Current state is counting down to off
+            self._count_down_off -= 1
+            if actual_sensor_value:
+                # New state is on
+                self._sensor_state = self._state_on
+            elif self._count_down_off > 0:
+                # State remains count down to off
+                pass
+            else:
+                # New state is off
+                self._sensor_state = self._state_off
+                self.sensor_value = False
+        else:
+            # Unknown state
+            pass
+
+        return self.sensor_value
--- pirtest.py
+++ pirtest.py
@@ -19,52 +19,84 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+from app_logger import AppLogger
 
-pir = 12
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pir, GPIO.IN)
+# Logger init
+the_app_logger = AppLogger("lumiclock")
+logger = the_app_logger.getAppLogger()
 
-try:
-    change_time = datetime.datetime.now()
-    last_v = GPIO.input(pir)
-    on_time = 0
-    display_state = False
-    default_on_time = 60 * 15
-    while True:
-        now = datetime.datetime.now()
-        v = GPIO.input(pir)
-        if last_v != v:
-            # Sensor changed
-            elapsed = now - change_time
-            change_time = now
-            if v:
-                # Sensor now on
-                on_time = default_on_time
-                display_state = True
-                print(now.strftime("%H:%M:%S"), v, "display turned on", elapsed.seconds)
+def old_main():
+    pir = 12
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pir, GPIO.IN)
+
+    try:
+        change_time = datetime.datetime.now()
+        last_v = GPIO.input(pir)
+        on_time = 0
+        display_state = False
+        default_on_time = 60 * 15
+        while True:
+            now = datetime.datetime.now()
+            v = GPIO.input(pir)
+            if last_v != v:
+                # Sensor changed
+                elapsed = now - change_time
+                change_time = now
+                if v:
+                    # Sensor now on
+                    on_time = default_on_time
+                    display_state = True
+                    print(now.strftime("%H:%M:%S"), v, "display turned on", elapsed.seconds)
+                else:
+                    # Sensor now off
+                    print(now.strftime("%H:%M:%S"), v, "for", elapsed.seconds)
             else:
-                # Sensor now off
-                print(now.strftime("%H:%M:%S"), v, "for", elapsed.seconds)
-        else:
-            # Sensor unchanged
-            if not v:
-                # Sensor off, run timer
-                if on_time:
-                    on_time -= 1
-                if on_time <= 0 and display_state:
-                    # display turns off
-                    display_state = False
-                    print(now.strftime("%H:%M:%S"), "display turned off", v)
-                else:    
-                    print(now.strftime("%H:%M:%S"), v, on_time)
-            else:
-                # Sensor on
-                on_time = default_on_time
-                print(now.strftime("%H:%M:%S"), v)
-        last_v = v
+                # Sensor unchanged
+                if not v:
+                    # Sensor off, run timer
+                    if on_time:
+                        on_time -= 1
+                    if on_time <= 0 and display_state:
+                        # display turns off
+                        display_state = False
+                        print(now.strftime("%H:%M:%S"), "display turned off", v)
+                    else:
+                        print(now.strftime("%H:%M:%S"), v, on_time)
+                else:
+                    # Sensor on
+                    on_time = default_on_time
+                    print(now.strftime("%H:%M:%S"), v)
+            last_v = v
+            time.sleep(1.0)
+    except:
+        print()
+        print("Keyboard interrupt")
+
+    print("End")
+
+def pir_update_handler(sensor_value):
+    # NOTE that this function is called on the sensor thread, NOT the main thread
+    now = datetime.datetime.now()
+    print(now.strftime("%H:%M:%S"), sensor_value)
+
+
+def main():
+    pir = 12
+    from pir_sensor_thread import SensorThread
+    threadinst = SensorThread(notify=pir_update_handler, pir_pin=12, time_off=300, time_on=10)
+    threadinst.start()
+
+    try:
         time.sleep(1.0)
-except:
-    print()
-    print("Keyboard interrupt")
+    except:
+        print()
+        print("Keyboard interrupt")
 
-print("End")
+    threadinst.terminate()
+
+    print("End")
+
+
+if __name__ == '__main__':
+    main()
